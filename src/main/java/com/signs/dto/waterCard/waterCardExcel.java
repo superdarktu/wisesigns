366dcc3d1625d4be30da1b0faa46e6401e8de937
superdarktu <695445293@qq.com>
2017-06-27 17:40:28 +0800

    啦啦

---
+++ src/main/java/com/signs/dto/waterCard/waterCardExcel.java
@@ -0,0 +1,24 @@
+package com.signs.dto.waterCard;
+
+public class waterCardExcel {
+
+    private String code;
+
+    private Integer type;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/signs/dto/waterFountain/WaterFountainExcel.java
@@ -0,0 +1,68 @@
+package com.signs.dto.waterFountain;
+
+import com.signs.util.ExcelNull;
+
+public class WaterFountainExcel {
+
+    private String code;
+
+    private String position;
+
+    private Integer type;
+
+    private String tableCode;
+
+    @ExcelNull
+    private Float longitude;
+
+    @ExcelNull
+    private Float latitude;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getTableCode() {
+        return tableCode;
+    }
+
+    public void setTableCode(String tableCode) {
+        this.tableCode = tableCode;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+}
