57ce38180456fb331ce7fc78e0f40a67304800b4
chenag <1053337648@qq.com>
2017-06-27 13:44:05 +0800

    嘿嘿嘿

---
+++ src/main/java/com/signs/controller/error/ErrorController.java
@@ -0,0 +1,20 @@
+package com.signs.controller.error;
+
+import com.signs.model.commons.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+@RestController
+@RequestMapping("/api/error")
+public class ErrorController {
+
+    @RequestMapping("/error")
+    public Result error(){
+        Result result = new Result();
+        result.setResult(5);
+        return result;
+    }
+}
