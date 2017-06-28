cdb3557a46c07a9240ae40238ff5aaf3f84b8987
chenag <1053337648@qq.com>
2017-06-28 14:13:42 +0800

    嘿嘿嘿

--- src/main/java/com/signs/controller/userPurchaseRecord/UserPurchaseRecordController.java
+++ src/main/java/com/signs/controller/userPurchaseRecord/UserPurchaseRecordController.java
@@ -12,12 +12,42 @@ import javax.annotation.Resource;
 import java.util.Date;
 
 @RestController
-@RequestMapping("/api/consume")
+@RequestMapping("/api/inventory")
 public class UserPurchaseRecordController {
 
     @Resource
     private UserPurchaseRecordService service;
 
+    /**
+     * 查询消费
+     */
+    @PostMapping("/consumption")
+    public Result pageCard(PageParam param, SearchVO dto) {
+        Result result = new Result();
+        try {
+            result.setData(service.page(param,dto.getId(),dto.getDate1(),dto.getDate2(),dto.getValue()));
+            result.setResult(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setResult(1);
+        }
+        return result;
+    }
 
+    /**
+     * 查询消费
+     */
+    @PostMapping("/getUserCount")
+    public Result  getUserCount(SearchVO vo){
+        Result result = new Result();
+        try {
+            result.setData(service.getUserCount(vo.getDate1()));
+            result.setResult(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setResult(1);
+        }
+        return result;
 
+    }
 }
