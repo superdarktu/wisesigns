2cdd39c4d1e371c9981bbc4be444a54cbab8f1d0
superdarktu <695445293@qq.com>
2017-06-30 13:10:33 +0800

    啦啦

---
+++ src/main/java/com/signs/dto/waterFountain/WaterFountainsVO.java
@@ -0,0 +1,14 @@
+package com.signs.dto.waterFountain;
+
+public class WaterFountainsVO {
+
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
