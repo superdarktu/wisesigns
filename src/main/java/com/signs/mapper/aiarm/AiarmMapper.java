9064c9650677b72e3affdd26e3a80e73fcc4b807
superdarktu <695445293@qq.com>
2017-06-30 10:09:21 +0800

    啦啦

--- src/main/java/com/signs/mapper/aiarm/AiarmMapper.java
+++ src/main/java/com/signs/mapper/aiarm/AiarmMapper.java
@@ -3,5 +3,10 @@ package com.signs.mapper.aiarm;
 import com.signs.model.aiarm.Aiarm;
 import com.signs.util.MyMapper;
 
+import java.util.List;
+import java.util.Map;
+
 public interface AiarmMapper extends MyMapper<Aiarm> {
+
+    List<Aiarm> pageData(Map<String,Object> map);
 }
