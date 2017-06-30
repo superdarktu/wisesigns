9b327f1b030b5e9d7ccc15fa81edfae583311262
superdarktu <695445293@qq.com>
2017-06-30 10:30:04 +0800

    啦啦

---
+++ src/main/java/com/signs/controller/aiarm/AiarmController.java
@@ -0,0 +1,90 @@
+package com.signs.controller.aiarm;
+
+
+import com.signs.dto.commonSearch.SearchVO;
+import com.signs.model.commons.PageParam;
+import com.signs.model.commons.Result;
+import com.signs.service.aiarm.AiarmService;
+import com.signs.service.waterFountains.WaterFountainsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/api/alert")
+public class AiarmController {
+
+    @Resource
+    private AiarmService service;
+
+    @Resource
+    private WaterFountainsService waterFountainsService;
+
+
+    /**
+     * 查询公用饮水机
+     * @return
+     */
+    @PostMapping("/information")
+    public Result information() {
+        Result result = new Result();
+
+        try {
+            result.setData(waterFountainsService.getPublicWaterFountains());
+        } catch (Exception e) {
+            result.setResult(1);
+        }
+        return result;
+    }
+
+    /*@PostMapping("/detailed")
+    public Result detailed() {
+        Result result = new Result();
+
+        try {
+            if (service.insert(collector))
+                result.setResult(0);
+        } catch (Exception e) {
+            result.setResult(1);
+        }
+        return result;
+    }*/
+
+    /**
+     * 分页查询
+     * @param param
+     * @param searchVO
+     * @return
+     */
+    @PostMapping("/svnStatus")
+    public Result svnStatus(PageParam param,SearchVO searchVO) {
+        Result result = new Result();
+
+        try {
+            result.setData(service.page(param,searchVO.getDate1(),searchVO.getDate2(),searchVO.getValue()));
+        } catch (Exception e) {
+            result.setResult(1);
+        }
+        return result;
+    }
+
+    /**
+     * 查询饮水机
+     * @param id
+     * @return
+     */
+    @PostMapping("/query")
+    public Result svnStatus(String id) {
+        Result result = new Result();
+
+        try {
+            result.setData(waterFountainsService.getSingleWaterFountains(id));
+        } catch (Exception e) {
+            result.setResult(1);
+        }
+        return result;
+    }
+
+}
